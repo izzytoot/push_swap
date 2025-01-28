d8650a8e49950ea3218a080161e0ff06d729586c
Isabel Cunha Tootill <icunha-t@c2r2s6.42porto.com>
2025-01-28 16:14:40 +0000

    PS

--- ft_init/error_handling.c
+++ ft_init/error_handling.c
@@ -68,6 +68,6 @@ void	ft_free_errors(t_stack_node **stack, char **array, int ac)
 	ft_free_stack(stack);
 	ft_printf("Error\n");
 	if (ac == 2)
-		free_split(array);
+		ft_free_split(array);
 	exit(1);
 }
